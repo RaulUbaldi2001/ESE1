c7dbbbadd6dbd6b23aca71cd2f408e49ce0c17c9
Isacco <Isacco@ISACCO-PC>
2018-01-24 14:16:00 +0100

    Modifiche2

---
+++ CicloFor1/CicloForConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CicloForConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            Console.WriteLine("Inserisci il numero di volte che la frase deve ripetersi");
+            a = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine("Lavoro Tpsit..");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
